958e1b466acf34f8bbfb4461f07a66ef3b0043b7
Vinicius Augutis <vinicius_augutis@hotmail.com>
2019-10-07 22:06:20 -0300

    Trabalhando com classes no es6

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-alert('TESTE');
+class List {
+    constructor() {
+        this.data = [];
+    }
 
-class Teste {
-    metodo() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class MatematicaStatic {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+class TodoList extends List {
+    constructor() {
+        super();
+        this.usuario = 'Vinicius';
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Oi');
+}
+
+MatematicaStatic.soma(1, 5);
